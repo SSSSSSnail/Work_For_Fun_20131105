d64c6e49545ea5d60a3da3abdde30653bf211ef9
Snail <warlocke@163.com>
2013-11-06 00:03:37 +0800

    init commit

---
+++ WFF_20131105/WFF_20131105/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WFF_20131105
+//
+//  Created by Snail on 5/11/13.
+//  Copyright (c) 2013 Snail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WFF_20131105/WFF_20131105/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WFF_20131105
+//
+//  Created by Snail on 5/11/13.
+//  Copyright (c) 2013 Snail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
